7491432b8b0a632ad06f9c6bda0250b0e59726a3
Hiruna15 <hirunadilmith15@gmail.com>
2024-12-29 19:56:40 +0530

    connected to the MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
-const app = express();
 import "dotenv/config";
+import connectDB from "./config/connectDB.js";
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`app is running on the port number ${PORT}`);
-});
+async function main() {
+  await connectDB();
+  console.log("Successfully connected to the MongoDB");
+  app.listen(PORT, () => {
+    console.log(`app is running on the port number ${PORT}`);
+  });
+}
+
+main();
